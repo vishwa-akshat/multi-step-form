71bdb16d4fc497dac075bf40c418955e4013d97b
Akshat Vishwakarma <akvs0707@gmail.com>
2023-01-27 18:59:50 +0530

    setup: svg to js conversion

---
+++ src/assets/images/icon-checkmark.js
@@ -0,0 +1,18 @@
+export default function CheckmarkIcon({ className }) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="9"
+      viewBox="0 0 12 9"
+    >
+      <path
+        fill="none"
+        stroke="#FFF"
+        stroke-width="2"
+        d="m1 4 3.433 3.433L10.866 1"
+      />
+    </svg>
+  );
+}
